8453c1828713bfceae6b9c64d0da51ff8c7e1fa8
wsgcy <gc.sg@163.com>
2018-11-06 21:40:08 +0800

    Merge branch 'master' of https://github.com/wsgcya/coder
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    modify:// 配置首页

---
+++ src/main/java/com/wsgcya/coder/common/config/DefultView.java
@@ -0,0 +1,17 @@
+package com.wsgcya.coder.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+@Configuration
+public class DefultView extends WebMvcConfigurationSupport {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
